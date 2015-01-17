102fbc86169494a802c0c3ea2dccb650bcd76111
Sam Ghods <sam@box.com>
2015-01-16 16:53:49 -0800

    Fix docker daemon not listening on all interfaces

--- setup.sh
+++ setup.sh
@@ -13,8 +13,10 @@ yum -y install yum-fastestmirror git mercurial subversion docker-io etcd-0.4.6-6
 # Docker setup.
 systemctl start docker
 systemctl enable docker
-# Make sure docker is listening on all network interfaces.
-echo " -H tcp://0.0.0.0:2375" >> /etc/sysconfig/docker
+# Set docker daemon comand line options. Keep in mind that at this point this
+# overrides any existing options. This is overridden to make sure docker is
+# listening on all network interfaces.
+echo "OPTIONS=--selinux-enabled -H tcp://0.0.0.0:2375" > /etc/sysconfig/docker
 # Supposedly you don't have to do this starting docker 1.0
 # (Fedora 20 is currently 1.1.2) but I found it necessary.
 usermod -a -G docker vagrant
